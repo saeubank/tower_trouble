56127a45782f51b16f320b470f7428de62139e74
swamulism <swamulism@gmail.com>
2017-06-23 11:21:35 -0600

    Updated map.h

--- src/map.h
+++ src/map.h
@@ -1,46 +1,8 @@
 #include "objects.h"
+#include <vector>
 
 #ifndef map_H
 #define map_H
-
-class Map
-{
-    int arr[81] = {0, 0, 0, 0, 0, 0, 0, 0, 0,  // it's a 2nd order Hilbert curve
-                      -1,-1,-1,-1, 0,-1,-1,-1,-1,
-                        0, 0, 0,-1, 0,-1, 0, 0, 0,
-                        0,-1,-1,-1, 0,-1,-1,-1, 0,
-                        0,-1, 0, 0, 0, 0, 0,-1, 0,
-                        0,-1, 0,-1,-1,-1, 0,-1, 0,
-                        0,-1, 0,-1, 0,-1, 0,-1, 0,
-                        0,-1,-1,-1, 0,-1,-1,-1, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int waves[9][10] = {{2, 2, 2, 2, 2, 0, 0, 0, 0, 0},
-                              {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-                              {1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-                              {2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
-                              {2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
-                              {3, 0, 3, 0, 3, 0, 3, 0, 3, 0},
-                              {3, 2, 1, 3, 2, 1, 3, 2, 1, 3},
-                              {3, 3, 3, 3, 2, 2, 2, 1, 1, 1},
-                              {3, 3, 3, 3, 3, 3, 3, 3, 3, 3}};
-    int wavetime = 600;
-    int currentwavetime = 0;
-    int spawncount = -1;
-public:
-    int lives = 20;
-    int currentwave = -1;
-    Map();
-    void tile(float x, float y, float z, int direction);
-    void render();
-    int animate();
-    void spawnwave();
-    int getlocation(float ex, float ey);
-};
-
-#endif
-
-
-#include <vector>
 
 struct Position {
 	int x, y;
@@ -79,20 +41,53 @@ struct PriorityQueue {
   }
 };
 
-class Map {
-	int width, height;
+class Map
+{
+    int arr[81] = {0, 0, 0, 0, 0, 0, 0, 0, 0,  // it's a 2nd order Hilbert curve
+                      -1,-1,-1,-1, 0,-1,-1,-1,-1,
+                        0, 0, 0,-1, 0,-1, 0, 0, 0,
+                        0,-1,-1,-1, 0,-1,-1,-1, 0,
+                        0,-1, 0, 0, 0, 0, 0,-1, 0,
+                        0,-1, 0,-1,-1,-1, 0,-1, 0,
+                        0,-1, 0,-1, 0,-1, 0,-1, 0,
+                        0,-1,-1,-1, 0,-1,-1,-1, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int waves[9][10] = {{2, 2, 2, 2, 2, 0, 0, 0, 0, 0},
+                              {2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                              {1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+                              {2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+                              {2, 2, 2, 2, 2, 1, 1, 1, 1, 1},
+                              {3, 0, 3, 0, 3, 0, 3, 0, 3, 0},
+                              {3, 2, 1, 3, 2, 1, 3, 2, 1, 3},
+                              {3, 3, 3, 3, 2, 2, 2, 1, 1, 1},
+                              {3, 3, 3, 3, 3, 3, 3, 3, 3, 3}};
+    int wavetime = 600;
+    int currentwavetime = 0;
+    int spawncount = -1;
+
+    int width, height;
 	std::vector<std::vector<Tile>> map;
 
 	void makeMap();
+public:
+    int lives = 20;
+    int currentwave = -1;
+    Map();
+    void tile(float x, float y, float z, int direction);
+    void render();
+    int animate();
+    void spawnwave();
+    int getlocation(float ex, float ey);
 
-	public:
-		Map(int width, int height);
-		int getWidth() const;
-		int getHeight() const;
-		Tile & operator[](Position pos);
-		const Tile & operator[](Position pos) const;
-		Tile getTile(Position pos) const;
-		void setTile(Tile tile, Position pos);
-		bool isEmpty(Position pos);
-		bool setTower(TileType tower, Position pos);
+    Map(int width, int height);
+    int getWidth() const;
+    int getHeight() const;
+    Tile & operator[](Position pos);
+    const Tile & operator[](Position pos) const;
+    Tile getTile(Position pos) const;
+    void setTile(Tile tile, Position pos);
+    bool isEmpty(Position pos);
+    bool setTower(TileType tower, Position pos);
 };
+
+#endif
