b5da1ba00db9de771475928e966a61ea09532ae8
Nelson <nelson@engr2gw-6-104-dhcp.int.colorado.edu>
2017-07-05 13:16:46 -0600

    Rendered highscores!

--- db/init.sql
+++ db/init.sql
@@ -6,26 +6,37 @@ create database scores;
 use scores;
 drop table highscores;
 -- we only need one simple table, primary hidden key id auto-increments so same user can have multiple highscores
-create table highscores (id int not null AUTO_INCREMENT, user VARCHAR(20), score int, PRIMARY KEY (id));
+create table highscores (id int not null AUTO_INCREMENT, user VARCHAR(6), score int, PRIMARY KEY (id));
 
 -- some dummy data to populate
-insert into highscores (user, score) values("nelson", 100000);
-insert into highscores (user, score) values("samuel", 1000000);
-insert into highscores (user, score) values("theodore",10000000);
-insert into highscores (user, score) values("jordan", 100000000);
+insert into highscores (user, score) values ("nelson", 100000),
+                                            ("samuel", 1000000),
+                                            ("theo",10000000),
+                                            ("jordan", 100000000);
 
 -- display the highscores in order of highest score so we know it worked
 select user, score from highscores order by score desc;
 
--- procedure to add then print highscores:
+-- clean up
 drop procedure add_score;
+drop procedure getScores;
+
+-- switch the delimiter for multiple-line procedures
 delimiter //
-CREATE PROCEDURE add_score(IN in_user VARCHAR(20), IN in_score int)
+
+-- insert user score
+CREATE PROCEDURE add_score(IN in_user VARCHAR(6), IN in_score int)
   BEGIN
   insert into highscores (user, score)
     values (in_user, in_score);
-  select user, score from highscores
-    order by score desc;
   END//
+
+-- get scores back as result set
+CREATE PROCEDURE getScores()
+    BEGIN
+    select user, score from highscores
+      order by score desc
+      limit 10;
+    END//
 
 delimiter ;
