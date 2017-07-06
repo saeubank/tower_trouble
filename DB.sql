17c338bf30327e3d27f86672927de5f96238ea6e
Mahalia E <maev8907@colorado.edu>
2017-07-05 21:29:00 -0600

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,9 +1,7 @@
 CREATE DATABASE tower is not exists;
 USE tower;
 CREATE TABLE if not exists `Players` (
- `Player Name` varchar(40) not null auto_increment,
- `Score` blob not null,
- primary key (`Names`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
-INSERT INTO 'Players'('Name', 'Score', 'Levels Completed') VALUES ('$Name', '$Score', '$LS');
+ `Player` integer NOT NULL AUTO_INCREMENT,
+ `Score` integer NOT NULL AUTO_INCREMENT);
+INSERT INTO 'Players'('Player', 'Score') VALUES ('$Name', '$Score');
 UPDATE 'Players';
