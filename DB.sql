31827f35e7017fa83fe9f5b1728a9a042d56b000
Mahalia E <maev8907@colorado.edu>
2017-07-05 21:33:57 -0600

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,7 +1,12 @@
 CREATE DATABASE tower is not exists;
 USE tower;
-CREATE TABLE if not exists `Players` (
- `Player` integer NOT NULL AUTO_INCREMENT,
- `Score` integer NOT NULL AUTO_INCREMENT);
-INSERT INTO 'Players'('Player', 'Score') VALUES ('$Name', '$Score');
+CREATE TABLE if not exists 'Players' (
+ 'Player' integer NOT NULL AUTO_INCREMENT,
+ 'Score' integer NOT NULL AUTO_INCREMENT);
+INSERT INTO 'Players' ('Player', 'Score') VALUES (1, 1000);
+INSERT INTO 'Players' ('Player', 'Score') VALUES (2, 900);
+INSERT INTO 'Players' ('Player', 'Score') VALUES (3, 250);
+INSERT INTO 'Players' ('Player', 'Score') VALUES (4, 70);
+INSERT INTO 'Players' ('Player', 'Score') VALUES (5, 30);
+# INSERT INTO 'Players'('Player', 'Score') VALUES ('$Name', '$Score');
 UPDATE 'Players';
