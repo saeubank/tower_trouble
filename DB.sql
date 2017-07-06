7cf4253a7c7c95f77b1be1924810abd43ce13ef5
Mahalia E <maev8907@colorado.edu>
2017-07-05 21:36:12 -0600

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE tower is not exists;
 USE tower;
 CREATE TABLE if not exists 'Players' (
- 'Player' integer NOT NULL AUTO_INCREMENT,
- 'Score' integer NOT NULL AUTO_INCREMENT);
+ 'Player' integer NOT NULL,
+ 'Score' integer NOT NULL);
 INSERT INTO 'Players' ('Player', 'Score') VALUES (1, 1000);
 INSERT INTO 'Players' ('Player', 'Score') VALUES (2, 900);
 INSERT INTO 'Players' ('Player', 'Score') VALUES (3, 250);
